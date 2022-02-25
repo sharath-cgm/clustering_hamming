c40c48b390a518f1d241cd55097c93059eb2d715
sharath-cgm <gurukulamitra@gmail.com>
2022-02-25 01:23:30 -0500

    Added kmeans.py with class def and seeding

---
+++ kmeans.py
@@ -0,0 +1,110 @@
+import random
+import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+# def lloyd_algo
+
+# def kmeans_plusplus: seeding method
+def D2_seeding(X, n_clusters, random_state = None, norm = 2):
+	"""
+ 	X : data
+
+	n_clusters : number of seeds to choose
+
+	norm : default 2, to generalize for l-p norm
+ 	---------
+
+ 	Returns centers
+ 	"""
+  
+	if random_state is not None:
+ 		np.random.seed(random_state)
+  
+	n_samples, n_features = X.shape
+	centers = np.empty((n_clusters, n_features), dtype=X.dtype)
+	n_local_trials = 2 + int(np.log(n_clusters))
+
+	# precompute
+	x_squared_norms = (X * X).sum(axis=1) ### 1
+
+	# Pick first center randomly
+	center_id = np.random.randint(0, n_samples-1) ##2
+	centers[0] = X[center_id]
+
+	# Initialize list of closest distances and calculate current potential
+	closest_dist_sq = euclidean_distances(
+		centers[0, np.newaxis], X, Y_norm_squared=x_squared_norms, squared=True
+	)
+	current_pot = closest_dist_sq.sum()
+	# print(closest_dist_sq, type(closest_dist_sq))
+
+
+	# Pick the remaining n_clusters-1 points
+	for c in range(1, n_clusters):
+		# Choose center candidates by sampling with probability proportional to the squared distance to the closest existing center
+		if norm == 2:
+			probability = closest_dist_sq
+		else:
+			probability = np.sqrt(closest_dist_sq)
+			# print(type(probability))
+			if(norm > 2):
+				probability = np.power(probability, norm)
+		# probability = closest_dist_sq
+
+		rand_vals = np.random.uniform(size=n_local_trials) * probability.sum()
+		candidate_ids = np.searchsorted(np.cumsum(probability), rand_vals) ##3
+		# np.clip(candidate_ids, None, closest_dist_sq.size - 1, out=candidate_ids)  ##4
+
+		# Compute distances to center candidates
+		distance_to_candidates = euclidean_distances(
+		  X[candidate_ids], X, Y_norm_squared=x_squared_norms, squared=True
+		)
+
+		# update closest distances squared and potential for each candidate
+		np.minimum(closest_dist_sq, distance_to_candidates, out=distance_to_candidates)
+		candidates_pot = distance_to_candidates.sum(axis=1)
+
+		# Decide which candidate is the best
+		best_candidate = np.argmin(candidates_pot)
+		current_pot = candidates_pot[best_candidate]
+		closest_dist_sq = distance_to_candidates[best_candidate]
+		best_candidate = candidate_ids[best_candidate]
+
+		# Add best center candidate found in local tries
+		centers[c] = X[best_candidate]
+
+	return centers
+
+
+class Kmeans:
+	def __init__(
+		self,
+		n_clusters = 2,
+		init = "k-means++",
+		n_init = 10,
+		max_iter = 300,
+		tol = 1e-4,
+		algorithm = "lloyd",
+		random_state = None
+	):
+		self.n_clusters = n_clusters
+		self.init = init
+		self.n_init = n_init
+		self.max_iter = max_iter
+		self.tol = tol
+		self.algorithm = algorithm
+		self.random_state = random_state
+
+	def _init_centroids():
+		
+
+	def fit
+
+	# def predict
+
+	def test_temp(self):
+		print(self.n_clusters)
+
+k = Kmeans()
+k.test_temp()
